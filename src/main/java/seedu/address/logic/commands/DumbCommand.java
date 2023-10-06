c9a13777944bd010daa9e0db7570d6a357616573
VN-Hao <111359733+VN-Hao@users.noreply.github.com>
2023-10-06 19:33:27 +0800

    Add Javadoc for the new dumb command

--- src/main/java/seedu/address/logic/commands/DumbCommand.java
+++ src/main/java/seedu/address/logic/commands/DumbCommand.java
@@ -2,10 +2,14 @@ package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
 
+/**
+ * Tells user that they have been dumbed.
+ */
 public class DumbCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult("Playing dumb!");
     }
 }
+
 
